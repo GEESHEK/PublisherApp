fe7251fdef447771f7acdc2fdfb575db94a3c7c5
Gee Chong Shek <geeshek95@outlook.com>
2023-09-07 16:53:27 +0100

    Many to many relationship created for Artist and Cover

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -9,6 +9,114 @@ using PublisherDomain;
 
 PubContext _context = new PubContext();
 
+ReassignACover();
+
+void ReassignACover()
+{
+    var coverwithartist4 = _context.Covers
+        .Include(c => c.Artists.Where(a => a.ArtistId == 4))
+        .FirstOrDefault(c => c.CoverId == 5);
+
+    coverwithartist4.Artists.RemoveAt(0);  
+    var artist3 = _context.Artists.Find(3);
+    coverwithartist4.Artists.Add(artist3);
+    // _context.ChangeTracker.DetectChanges();
+    //need to call saveChanges here
+}
+
+void UnAssignAnArtistFromACover()
+{
+    var coverwithartist = _context.Covers
+        .Include(c => c.Artists.Where(a => a.ArtistId == 1))
+        .FirstOrDefault(c => c.CoverId == 1);
+    coverwithartist.Artists.RemoveAt(0);
+    // _context.Artists.Remove(coverwithartist.Artists[0]);
+    _context.ChangeTracker.DetectChanges();
+    // var debugview = _context.ChangeTracker.DebugView.ShortView;
+    _context.SaveChanges();
+}
+
+void RetrieveAllArtistsWhoHaveCovers()
+{
+    var artistsWithCovers = _context.Artists.Where(a => a.Covers.Any()).ToList();
+}
+
+void RetrieveAllArtistsWithTheirCovers()
+{
+    var artistsWithCovers = _context.Artists.Include(a => a.Covers).ToList();
+  
+    foreach (var a in artistsWithCovers)
+    {
+        Console.WriteLine($"{a.FirstName} {a.LastName}, Designs to work on:");
+        var primaryArtistId = a.ArtistId;
+        if (a.Covers.Count() == 0)
+        {
+            Console.WriteLine("  No covers");
+        }
+        else
+        {
+            foreach (var c in a.Covers)
+            {
+                string collaborators = "";
+                foreach (var ca in c.Artists.Where(ca => ca.ArtistId != primaryArtistId))
+                {
+                    collaborators += $"{ca.FirstName} {ca.LastName}";
+                }
+                if (collaborators.Length > 0)
+                { collaborators = $"(with {collaborators})"; }
+                Console.WriteLine($"  *{c.DesignIdeas} {collaborators}");
+            }
+        }
+    }
+}
+
+void RetrieveACoverWithItsArtists()
+{
+    var coverWithArtists = _context.Covers.Include(c => c.Artists)
+        .FirstOrDefault(c => c.CoverId == 1);
+}
+
+void RetrieveAnArtistWithTheirCovers()
+{
+    var artistWithCovers = _context.Artists.Include(a => a.Covers)
+        .FirstOrDefault(a => a.ArtistId == 1);
+}
+
+void FindArtist()
+{
+    var artistA = _context.Artists.Find(1);
+    Console.WriteLine("------------------------ " + artistA.FirstName);
+}
+
+void CreateNewCoverAndArtistTogether()
+{
+    var newArtist = new Artist { FirstName = "Kir", LastName = "Talmage" };
+    var newCover = new Cover { DesignIdeas = "We like birds!"};
+    newArtist.Covers.Add(newCover);
+    _context.Artists.Add(newArtist);
+    _context.SaveChanges();
+}
+
+void CreateNewCoverWithExistingArtist()
+{
+    var artistA = _context.Artists.Find(1);
+    var cover = new Cover { DesignIdeas = "author has provide a photo" };
+    cover.Artists.Add(artistA);
+    _context.Covers.Add(cover);
+    _context.SaveChanges();
+}
+
+void ConnectExistingArtistAndCoverObject()
+{
+    var artistA = _context.Artists.Find(1);
+    var artistB = _context.Artists.Find(2);
+    var coverA = _context.Covers.Find(1);
+    coverA.Artists.Add(artistA);
+    coverA.Artists.Add(artistB);
+    _context.SaveChanges();
+}
+
+
 void CascadeDeleteInActionWhenTracked()
 {
     //note : I knew that author with id 2 had books in my sample database
@@ -261,7 +369,7 @@ void VariousOperations()
     _context.SaveChanges();
 }
 
-void RestrieveAndUpdateAuthor()
+void RestiveAndUpdateAuthor()
 {
     var author = _context.Authors.FirstOrDefault(a => a.FirstName == "Julie" && a.LastName == "Lerman");
     if (author != null)
