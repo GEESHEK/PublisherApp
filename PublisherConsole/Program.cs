838b4b42953984515ccf9e1cf06827f128f42942
Gee Chong Shek <geeshek95@outlook.com>
2023-09-08 11:46:26 +0100

    One to one relationship implemented

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -9,7 +9,91 @@ using PublisherDomain;
 
 PubContext _context = new PubContext();
 
-ReassignACover();
+//this will not work, already has a cover associated with it and we aren't tracking cover here
+void AddCoverToExistingBookThatHasAnUnTrackedCover()
+{
+    var book = _context.Books.Find(5); //The Never
+    book.Cover = new Cover { DesignIdeas = "A spiral" };
+    _context.SaveChanges();
+}
+
+//any dependent also needs to be in memory and tracked i.e. the book
+void AddCoverToExistingBookWithTrackedCover()
+{
+    var book = _context.Books.Include(b => b.Cover)
+        .FirstOrDefault(b => b.BookId == 5); //The Never
+    book.Cover = new Cover { DesignIdeas = "A spiral" };
+    _context.ChangeTracker.DetectChanges();
+    // var debugview = _context.ChangeTracker.DebugView.ShortView;
+}
+
+//To be more explicitly 
+void ProtectingFromUniqueFK()
+{
+    var TheNeverDesignIdeas = "A spirally spiral";
+    var book = _context.Books.Include(b => b.Cover)
+        .FirstOrDefault(b => b.BookId == 5); //The Never
+    if (book.Cover != null)
+    {
+        book.Cover.DesignIdeas = TheNeverDesignIdeas;
+    }
+    else
+    {
+        book.Cover = new Cover { DesignIdeas = "A spirally spiral" };
+    }
+    _context.SaveChanges();
+}
+
+void NewBookAndCover()
+{
+    var book = new Book
+    {
+        AuthorId = 1, Title = "Call Me Ishtar", PublishDate = new DateTime(1973, 1, 1)
+    };
+    book.Cover = new Cover { DesignIdeas = "image of Ishtar" };
+    _context.Books.Add(book);
+    _context.SaveChanges();
+}
+
+void AddCoverToExistingBook()
+{
+    var book = _context.Books.Find(7); //Wool
+    book.Cover = new Cover { DesignIdeas = "A wool scouring pad" };
+    _context.SaveChanges();
+}
+
+void MultiLevelInclude()
+{
+    //avoid paying the price for all the change-tracking 
+    var authorGraph = _context.Authors.AsNoTracking()
+        .Include(a => a.Books)
+        .ThenInclude(b => b.Cover)
+        .ThenInclude(c => c.Artists)
+        .FirstOrDefault(a => a.AuthorId == 1);
+    
+    Console.WriteLine(authorGraph?.FirstName + " " + authorGraph?.LastName);
+    foreach (var book in authorGraph.Books)
+    {
+        Console.WriteLine("Book:" + book.Title);
+        if (book.Cover != null)
+        {
+            Console.WriteLine("Design Ideas: " + book.Cover.DesignIdeas);
+            Console.Write("Artist(s):");
+            book.Cover.Artists.ForEach(a => Console.Write(a.LastName + " "));
+
+        }
+    }
+}
+
+
+void GetAllBooksWithTheirCovers()
+{
+    var booksandcovers = _context.Books.Include(b => b.Cover).ToList();
+    booksandcovers.ForEach(book => 
+        Console.WriteLine(
+            book.Title + 
+            (book.Cover == null ? ": No cover yet" : ":" + book.Cover.DesignIdeas)));
+}
 
 void ReassignACover()
 {
